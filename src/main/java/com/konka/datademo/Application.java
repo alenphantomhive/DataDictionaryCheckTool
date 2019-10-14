ff051b23fae18750531147263699009298e925ff
alenphantomhive <920972785@qq.com>
2019-10-14 14:33:43 +0800

    Revert "demo"
    This reverts commit 0b84d56784f5b8fa01eb43d00262e28929d0d829.

--- src/main/java/com/konka/datademo/Application.java
+++ src/main/java/com/konka/datademo/Application.java
@@ -6,12 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 @MapperScan(basePackages = "com.konka.datademo.mapper")
-@EnableTransactionManagement
 public class Application extends SpringBootServletInitializer {
 
 	@Override
